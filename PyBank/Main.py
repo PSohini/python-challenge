4593485d69a3320f8f486a9e1bbd990768d1e898
Sim City Builder <sohini.ponnam@gmail.com>
2018-10-20 15:53:19 -0500

    PyBank

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -59,3 +59,13 @@ output = (
           f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
           f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
 print(output)
+
+#Write output file
+# Set variable for output file
+output_file = os.path.join("pybank_final.csv")
+
+# Display results on output file
+with open(output_file, "w") as txt_file:
+   txt_file.write(output)
+
+    
