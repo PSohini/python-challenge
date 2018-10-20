f23ec827d5d866972be7da4a70a8b286001bba79
Sim City Builder <sohini.ponnam@gmail.com>
2018-10-19 21:33:23 -0500

    PyBankHomework

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -2,36 +2,22 @@
 import os
 #Module for reading csv files
 import csv
+TotalMonths = 0
+Monthofchange=[]
+Notchange=[]
+greatest_increase = ["", 0]
+greatest_decrease = ["", 9999999999999999999]
+total_net = 0
+
 
 #Files to load and output
-file_to_load = os.path.join('..','PyBank','budget_data.csv')
-file_to_output = os.path.join('..','Pybank','Budget_Analysis.txt')
-# Method 2: Improved Reading using CSV module
-
+file_to_load = os.path.join('budget_data.csv')
+print(file_to_load)
 with open(file_to_load, newline='') as csvfile:
-
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
+# Set a variable to header and set that to next csv reader
+    Header=next(csvreader)
 
-    print(csvreader)
+    print(Header)
 
-    # Read the header row first (skip this step if there is now header)
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
-    # Read each row of data after the header
-    for row in csvreader:
-        print(row)
-
-    print("let's do it again")
-
-    for row in csvreader:
-        print(row)
-
-
-# Print all of the results (to terminal)
-print(output)
-
-# Save the results to analysis text file
-#with open(file_to_output, "a") as txt_file:
- #   txt_file.write(output)
